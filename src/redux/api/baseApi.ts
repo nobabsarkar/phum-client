9455d8512db0b9fd9c7b48893adaed85a6f3a2c4
nobabsarkar <nobabsarkar2016@gmail.com>
2025-02-05 13:03:08 +0600

    add code

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -29,7 +29,6 @@ const baseQueryWithRefreshToken: BaseQueryFn<
   DefinitionType
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
-  console.log(result);
 
   if (result?.error?.status === 401) {
     // send Refresh

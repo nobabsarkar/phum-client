fa73983e23e58d1dee518999e35445a92ed9bddf
nobabsarkar <nobabsarkar2016@gmail.com>
2025-07-10 22:56:08 +0600

    get offered course data

--- src/pages/admin/courseManagement/OfferedCourses.tsx
+++ src/pages/admin/courseManagement/OfferedCourses.tsx
@@ -1,28 +1,28 @@
 import { Table, TableColumnsType } from "antd";
 import { OfferedCourseTableRow } from "./Course";
-// import { useOfferedCourseQuery } from "../../../redux/features/admin/courseManagement";
-// import { TOfferedCourse } from "../../../types/studentCourse.type";
+import { useOfferedCourseQuery } from "../../../redux/features/admin/courseManagement";
+import { TOfferedCourse } from "../../../types/studentCourse.type";
 
 const OfferedCourses = () => {
-  // const { data: offeredData, isFetching } = useOfferedCourseQuery(undefined);
+  const { data: offeredData, isFetching } = useOfferedCourseQuery(undefined);
 
-  // const tableData = Array.isArray(offeredData?.data?.result)
-  //   ? (offeredData?.data?.result as TOfferedCourse[])?.map(
-  //       ({
-  //         _id,
-  //         academicDepartment,
-  //         semesterRegistration,
-  //         academicSemester,
-  //         academicFaculty,
-  //       }) => ({
-  //         key: _id,
-  //         academicDepartment: academicDepartment?.name,
-  //         semesterRegistration: semesterRegistration?.status,
-  //         academicSemester: academicSemester?.name,
-  //         academicFaculty: academicFaculty?.name,
-  //       })
-  //     )
-  //   : [];
+  const tableData = Array.isArray(offeredData?.data?.result)
+    ? (offeredData?.data?.result as TOfferedCourse[])?.map(
+        ({
+          _id,
+          academicDepartment,
+          semesterRegistration,
+          academicSemester,
+          academicFaculty,
+        }) => ({
+          key: _id,
+          academicDepartment: academicDepartment?.name,
+          semesterRegistration: semesterRegistration?.status,
+          academicSemester: academicSemester?.name,
+          academicFaculty: academicFaculty?.name,
+        })
+      )
+    : [];
 
   const columns: TableColumnsType<OfferedCourseTableRow> = [
     {
@@ -45,22 +45,22 @@ const OfferedCourses = () => {
       key: "academic faculty",
       dataIndex: "academicFaculty",
     },
-    // {
-    //   title: "Action",
-    //   key: "x",
-    //   render: (item) => {
-    //     // return <AddFacultyModel facultyInfo={item} />;
-    //     return <h1>Offered Course</h1>;
-    //   },
-    // },
+    {
+      title: "Action",
+      key: "x",
+      render: (item) => {
+        return <AddFacultyModel facultyInfo={item} />;
+        return <h1>Offered Course</h1>;
+      },
+    },
   ];
 
   return (
     <div>
       <Table
-        // loading={isFetching}
+        loading={isFetching}
         columns={columns}
-        // dataSource={tableData}
+        dataSource={tableData}
         // onChange={onChange}
       />
     </div>
